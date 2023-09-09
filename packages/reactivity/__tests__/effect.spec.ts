86936a12f4c8999a76446a91d72b7b815cb9a7ab
pkc918 <2489964425@qq.com>
2023-09-09 16:32:31 +0800

    test: added scheduler unit test

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { reactive } from "../reactive";
 import { effect } from "../effect";
 
 describe("test: effect", () => {
-    it('should access', () => {
+    it("should access", () => {
         const user = reactive<{
             age: number
         }>({
@@ -16,5 +16,38 @@ describe("test: effect", () => {
         expect(nextAge).toBe(11);
         user.age++;
         expect(nextAge).toBe(12);
+    });
+
+    it("should return runner when call effect", () => {
+        const fn = vi.fn();
+        const runner = effect(() => {
+            fn();
+            return "You have to tell me the truthy";
+        });
+        expect(fn).toBeCalledTimes(1);
+        const res = runner();
+        expect(fn).toBeCalledTimes(2);
+        expect(res).toBe("You have to tell me the truthy");
+    });
+
+    it("should call scheduler when it not undefined", () => {
+        let dummy: any;
+        let run: any;
+        const scheduler = vi.fn(() => {
+            run = runner
+        });
+        const obj = reactive({
+            foo: 10
+        });
+        const runner = effect(() => {
+            dummy = obj.foo;
+        }, {scheduler});
+        expect(dummy).toBe(10)
+        expect(scheduler).not.toBeCalled();
+        obj.foo++;
+        expect(dummy).toBe(10)
+        expect(scheduler).toBeCalledTimes(1)
+        run();
+        expect(dummy).toBe(11)
     });
 });
