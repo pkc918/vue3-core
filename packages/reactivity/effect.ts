73db0858c6cf1e019bbb89224e43dc1a3d54a6aa
pkc918 <2489964425@qq.com>
2023-09-09 16:32:12 +0800

    feat: added scheduler in effect

--- packages/reactivity/effect.ts
+++ packages/reactivity/effect.ts
@@ -1,25 +1,31 @@
 export type Deps = Set<ReactiveEffect>
 export type DepMap = Map<string | symbol, Deps>
 export type Bucket = WeakMap<object, DepMap>
+export interface EffectOption{
+    scheduler?: Function
+}
 
 let activeEffect: ReactiveEffect;
 
 class ReactiveEffect {
+    public _scheduler?: Function;
     private readonly _fn: Function;
 
-    constructor(fn: Function) {
+    constructor(fn: Function, scheduler?: Function) {
         this._fn = fn;
+        this._scheduler = scheduler;
     }
 
     run() {
         activeEffect = this;
-        this._fn();
+        return this._fn();
     }
 }
 
-export function effect(fn: Function) {
-    const _effect = new ReactiveEffect(fn);
+export function effect(fn: Function, options: EffectOption = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler);
     _effect.run();
+    return _effect.run.bind(_effect);
 }
 
 const bucket: Bucket = new WeakMap();
@@ -40,5 +46,10 @@ export function trigger(target: object, key: string | symbol) {
     const depMap = bucket.get(target);
     if (!depMap) return;
     const deps = depMap.get(key);
-    deps && deps.forEach(effect => effect.run());
+    deps && deps.forEach(effect => {
+        if (effect._scheduler){
+            return effect._scheduler();
+        }
+        effect.run()
+    });
 }
