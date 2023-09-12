948024f585a5ff10f3c23b025ae6a3b1ef4a0aba
pkc918 <xiaopan2000918@gmail.com>
2023-09-12 19:12:51 +0800

    test: added shallowReadonly event unit test

---
+++ packages/reactivity/__tests__/shallowReadonly.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test, vi } from "vitest";
+import { isReadonly, readonly, shallowReadonly } from "../reactive";
+
+describe("test: shallowReadonly", () => {
+    test("should not make non-reactive properties reactive", () => {
+        const prop = shallowReadonly({user: {val: 1}});
+        expect(isReadonly(prop)).toBe(true);
+        expect(isReadonly(prop.user)).toBe(false);
+    });
+
+    test("warn when call set", () => {
+        console.warn = vi.fn();
+        const original = shallowReadonly({
+            val: 1
+        });
+        original.val++;
+        expect(console.warn).toBeCalled();
+    });
+});
